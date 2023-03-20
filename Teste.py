0debbbdbe3bc6667c18e3e2d378fa5fd65ccd455
StiigGit <ticianoodonto@gmail.com>
2023-03-20 00:51:37 -0300

    Initial commit

---
+++ Teste.py
@@ -0,0 +1,26 @@
+#Criado por Ticiano#
+#2023-03-19#
+
+
+nome = "Eu amo programar com a linguagem Python"
+print(nome)
+nome=input("por favor, digite o seu nome: ")
+print(f'Olá, Sr {nome}, bom dia!')
+
+numero = float(input("Digite um número: "))
+numero = numero + 1357
+numero = numero * 8
+numero = numero / 5
+numero = numero ** 2
+print(numero)
+
+nome=input("por favor, digite o seu nome completo: ")
+nome=nome.upper()
+print(f'Olá Sr(a) {nome}, você acaba se ser convidado para uma festa!')
+
+numero = float(input("Digite um número: "))
+numero = numero * 2
+numero = numero + 16
+numero = numero / 2
+numero = numero - numero
+print(numero)
