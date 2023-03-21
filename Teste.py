918a4dbbb8b9484de8041f040a9e8abe0297c635
Ticiano <126998734+StiigGit@users.noreply.github.com>
2023-03-20 23:47:11 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -18,9 +18,9 @@ nome=input("por favor, digite o seu nome completo: ")
 nome=nome.upper()
 print(f'Olá Sr(a) {nome}, você acaba se ser convidado para uma festa!')
 
-numero = float(input("Digite um número: "))
-numero = numero * 2
+numero1 = float(input("Digite um número: "))
+numero = numero1 * 2
 numero = numero + 16
 numero = numero / 2
-numero = numero - numero
+numero = numero - numero1
 print(numero)
