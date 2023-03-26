b2a976c32584e51aa9fa5ab5f1bfc369b1cd3b0e
Ticiano <126998734+StiigGit@users.noreply.github.com>
2023-03-26 18:43:30 -0300

    Exercícios TED3
    Exercícios primeira etapa P1B

---
+++ Ted3.0/Questão 4.py
@@ -0,0 +1,4 @@
+nome = input("Olá, por favor digite seu nome: ")
+nome = nome.upper()
+
+print(f'Olá {nome}, você está convidado para o meu aniversário!' )
---
+++ Ted3.0/Questão 5.py
@@ -0,0 +1,6 @@
+numero1 = float(input("Digite um número: "))
+numero = numero1 * 2
+numero = numero + 16
+numero = numero / 2
+numero = numero - numero1
+print(numero)
